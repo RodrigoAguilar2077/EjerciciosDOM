90cd026a4f46f87a2f7ed010c3442d197b579e34
RodrigoAguilar2077 <rodrigo.aguilar.flores4@gmail.com>
2026-01-29 14:05:18 -0600

    Resaltar tarjetas

--- Lab1DOM8a26-main/app.js
+++ Lab1DOM8a26-main/app.js
@@ -27,3 +27,21 @@ btnCambiar.addEventListener('click', () => {
     titulo.dataset.alt = alt ? '0' : '1';
     setEstado('Mensaje cambiado');
 });
+
+//manejador del evento mouseover de los artículos
+const listaArticulos = $('#listaArticulos');
+
+listaArticulos.addEventListener('mouseover', (e) => {
+    const card = e.target.closest('.card');
+    if (!card)
+        return;
+    card.classList.add('is-highlight');
+});
+
+//manejador del evento mouseout de los artículos
+listaArticulos.addEventListener('mouseout', (e) => {
+    const card = e.target.closest('.card');
+    if (!card)
+        return;
+    card.classList.remove('is-highlight');
+});
